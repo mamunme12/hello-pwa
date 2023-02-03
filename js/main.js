b603ef9d07915420997efcbe58ca3768d0993cb6
mamunme12 <mamun@evolany.com>
2023-02-03 17:37:41 +0900

    ask for notification permission

--- js/main.js
+++ js/main.js
@@ -5,4 +5,33 @@ window.onload = () => {
     navigator.serviceWorker
              .register('./sw.js');
   }
+
+  if ('Notification' in window && Notification.permission != 'granted') {
+    console.log('Ask user permission')
+    Notification.requestPermission(status => {  
+        console.log('Status:'+status)
+        displayNotification('Notification Enabled');
+    });
+}
+
+
+const displayNotification = notificationTitle => {
+    console.log('display notification')
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(reg => {
+            console.log(reg)
+            const options = {
+                    body: 'Thanks for allowing push notification !',
+                    icon:  '/assets/icons/icon-512x512.png',
+                    vibrate: [100, 50, 100],
+                    data: {
+                      dateOfArrival: Date.now(),
+                      primaryKey: 0
+                    }
+                  };
+    
+            reg.showNotification(notificationTitle, options);
+        });
+    }
+};
 }
